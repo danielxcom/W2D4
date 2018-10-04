dbadb8d867c8cedc1b9a905eb3444a19a74246e7
danielxcom <danixu12@gmail.com>
2018-10-04 15:29:41 -0400

    sum equation 1

--- execution_time_complexity.rb
+++ execution_time_complexity.rb
@@ -72,3 +72,43 @@ def min_linear(list)
 
   smallest
 end
+
+# II Largest contiguous sub-sum
+# list = [5, 3, -7]
+#     largest_contiguous_subsum(list) # => 8
+#
+#     # possible sub-sums
+#     [5]           # => 5
+#     [5, 3]        # => 8 --> we want this one
+#     [5, 3, -7]    # => 1
+#     [3]           # => 3
+#     [3, -7]       # => -4
+#     [-7]          # => -7
+
+# O(n^2)
+def sub_sum(set)
+  store = []
+
+  (0...set.length).each do |idx|
+    (0...set.length).each do |jdx|
+      store << set[idx..jdx]
+    end
+  end
+  amount = 0
+  highest = 0
+
+  store.each do |el|
+
+    el.each do |sum|
+      amount += sum
+    end
+
+    highest = amount if highest < amount
+    amount = 0
+  end
+
+  highest
+end
+
+# list = [5,3,-7]
+# sub_sum(list)
