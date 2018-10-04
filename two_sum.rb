55c23c615714b94e9236bea3d8ff9f060408559c
danielxcom <danixu12@gmail.com>
2018-10-04 17:30:19 -0400

    brute force twosum

---
+++ two_sum.rb
@@ -0,0 +1,19 @@
+# O(n) as it is iterative, straight forward array loop.
+# O(n^2) Quadratic, now we compare the two loops and find
+# something that matches target sum.
+# idx+1 to never check same pair. its slower because it has more
+# loops = more steps.
+
+def bad_two_sum(arr, target_sum)
+
+  (0...arr.length).each do |idx|
+    el = arr[idx]
+    (idx+1...arr.length).each do |jdx|
+      el2 = arr[jdx]
+      puts "#{el} + #{el2}"
+      return true if el + el2 == target_sum
+    end
+  end
+
+  return false
+end
