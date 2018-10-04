2b60082c0505f48bc04286080b577da4c324e1c8
danielxcom <danixu12@gmail.com>
2018-10-04 16:52:19 -0400

    Figured the hash was the fourth_anagrams?(...) and created second_anagram?(...)

--- anagrams.rb
+++ anagrams.rb
@@ -1,39 +1,4 @@
-# anagram?("gizmo", "sally")    #=> false
 
-# O(n^3) Cubic time space complexity
-# Reason: Nested for loop with additional 2 loops to settle the strings
-# into hash map.
-def anagram?(str1, str2)
-  hash1 = Hash.new
-  hash2 = Hash.new
-  if str1.length != str2.length
-    return false
-  end
-
-  str1.each_char do |el|
-    if hash1[el].nil?
-      hash1[el] = 1
-    else
-      hash1[el] += 1
-    end
-  end
-
-  str2.each_char do |el|
-    if hash2[el].nil?
-      hash2[el] = 1
-    else
-      hash2[el] += 1
-    end
-  end
-
-  hash1.each do |key, item|
-    hash2.each do |key, item|
-      return false if hash1[key] != hash2[key]
-    end
-  end
-
-  return true
-end
 
 # anagram?("elvis", "lives")    #=> true
 
@@ -65,4 +30,69 @@ def permutations(string)
   end
 
   result
+end
+
+#second_anagram? iterates over the first string...
+#find_index, delete.
+#O(n^2) Quadratic Time because of the n steps multiplying by two strings
+# and then we find specific index with string-char.
+# associated with this.
+
+def second_anagram?(str1, str2)
+  arr1 = str1.split('')
+  arr2 = str2.split('')
+
+  if str1.length != str2.length
+    return false
+  end
+
+  (0...arr1.length).each do |idx|
+    letter = arr1[idx]
+    target = arr2.find_index(letter)
+    arr2.delete(arr2[target])
+  end
+
+  return true if arr2.empty?
+end
+
+#O(n logn) because Sorting was mentioned.
+
+# anagram?("gizmo", "sally")    #=> false
+
+# O(n^3) Cubic time space complexity
+# Reason: Nested for loop with additional 2 loops to settle the strings
+# into hash map.
+# this turns into a O(1) complexity as we check along two hashes.
+# EDIT: We don't need to do .each for both, just check the two hashes directly. 
+def fourth_anagram?(str1, str2)
+  hash1 = Hash.new
+  hash2 = Hash.new
+  if str1.length != str2.length
+    return false
+  end
+
+  str1.each_char do |el|
+    if hash1[el].nil?
+      hash1[el] = 1
+    else
+      hash1[el] += 1
+    end
+  end
+
+  str2.each_char do |el|
+    if hash2[el].nil?
+      hash2[el] = 1
+    else
+      hash2[el] += 1
+    end
+  end
+
+  # hash1.each do |key, item|
+  #   hash2.each do |key, item|
+  #     return false if hash1[key] != hash2[key]
+  #   end
+  # end
+  #
+  # return true
+  hash1 == hash2
 end
